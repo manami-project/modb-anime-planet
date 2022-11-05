18ca833ac21b5953b10a04def695395d629cb068
manami-project <13179375+manami-project@users.noreply.github.com>
2022-11-05 11:49:22 +0100

    Updated dependencies

--- src/test/kotlin/io/github/manamiproject/modb/animeplanet/AnimePlanetDownloaderTest.kt
+++ src/test/kotlin/io/github/manamiproject/modb/animeplanet/AnimePlanetDownloaderTest.kt
@@ -15,7 +15,7 @@ import io.github.manamiproject.modb.test.*
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.Test
+import kotlin.test.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 import java.net.URI
@@ -30,34 +30,34 @@ internal class AnimePlanetDownloaderTest : MockServerTestCase<WireMockServer> by
 
     @Test
     fun `successfully download an anime`() {
-        // given
-        val testConfig = object: MetaDataProviderConfig by MetaDataProviderTestConfig {
-            override fun hostname(): Hostname = "localhost"
-            override fun buildDataDownloadLink(id: String): URI = URI("http://localhost:$port/anime/$id")
-        }
+        runBlocking {
+            // given
+            val testConfig = object : MetaDataProviderConfig by MetaDataProviderTestConfig {
+                override fun hostname(): Hostname = "localhost"
+                override fun buildDataDownloadLink(id: String): URI = URI("http://localhost:$port/anime/$id")
+            }
 
-        val id = "black-clover"
+            val id = "black-clover"
 
-        serverInstance.stubFor(
-            get(urlPathEqualTo("/anime/$id")).willReturn(
-                aResponse()
-                    .withHeader("Content-Type", APPLICATION_JSON)
-                    .withStatus(200)
-                    .withBody("<html>content for $id</html>")
+            serverInstance.stubFor(
+                get(urlPathEqualTo("/anime/$id")).willReturn(
+                    aResponse()
+                        .withHeader("Content-Type", APPLICATION_JSON)
+                        .withStatus(200)
+                        .withBody("<html>content for $id</html>")
+                )
             )
-        )
 
-        val downloader = AnimePlanetDownloader(testConfig)
+            val downloader = AnimePlanetDownloader(testConfig)
 
-        // when
-        val result = runBlocking {
-            downloader.download(id) {
+            // when
+            val result = downloader.download(id) {
                 shouldNotBeInvoked()
             }
-        }
 
-        // then
-        assertThat(result).isEqualTo("<html>content for black-clover</html>")
+            // then
+            assertThat(result).isEqualTo("<html>content for black-clover</html>")
+        }
     }
 
     @Test
@@ -128,88 +128,88 @@ internal class AnimePlanetDownloaderTest : MockServerTestCase<WireMockServer> by
     @ParameterizedTest
     @ValueSource(ints = [429, 500, 502, 521, 525])
     fun `pause and retry on response code`(responseCode: Int) {
-        // given
-        val id = 1535
+        runBlocking {
+            // given
+            val id = 1535
 
-        val testAnimePlanetConfig = object: MetaDataProviderConfig by MetaDataProviderTestConfig {
-            override fun hostname(): Hostname = "localhost"
-            override fun buildAnimeLink(id: AnimeId): URI = AnimePlanetConfig.buildAnimeLink(id)
-            override fun buildDataDownloadLink(id: String): URI = URI("http://localhost:$port/anime/$id")
-            override fun fileSuffix(): FileSuffix = AnimePlanetConfig.fileSuffix()
-        }
+            val testAnimePlanetConfig = object : MetaDataProviderConfig by MetaDataProviderTestConfig {
+                override fun hostname(): Hostname = "localhost"
+                override fun buildAnimeLink(id: AnimeId): URI = AnimePlanetConfig.buildAnimeLink(id)
+                override fun buildDataDownloadLink(id: String): URI = URI("http://localhost:$port/anime/$id")
+                override fun fileSuffix(): FileSuffix = AnimePlanetConfig.fileSuffix()
+            }
 
-        serverInstance.stubFor(
+            serverInstance.stubFor(
                 get(urlPathEqualTo("/anime/$id"))
-                        .inScenario("pause and retry")
-                        .whenScenarioStateIs(STARTED)
-                        .willSetStateTo("successful retrieval")
-                        .willReturn(
-                                aResponse()
-                                        .withHeader("Content-Type", "text/html")
-                                        .withStatus(responseCode)
-                                        .withBody("<html></html>")
-                        )
-        )
+                    .inScenario("pause and retry")
+                    .whenScenarioStateIs(STARTED)
+                    .willSetStateTo("successful retrieval")
+                    .willReturn(
+                        aResponse()
+                            .withHeader("Content-Type", "text/html")
+                            .withStatus(responseCode)
+                            .withBody("<html></html>")
+                    )
+            )
 
-        val responseBody = "<html><head/><body></body></html>"
+            val responseBody = "<html><head/><body></body></html>"
 
-        serverInstance.stubFor(
+            serverInstance.stubFor(
                 get(urlPathEqualTo("/anime/$id"))
-                        .inScenario("pause and retry")
-                        .whenScenarioStateIs("successful retrieval")
-                        .willReturn(
-                                aResponse()
-                                        .withHeader("Content-Type", APPLICATION_JSON)
-                                        .withStatus(200)
-                                        .withBody(responseBody)
-                        )
-        )
+                    .inScenario("pause and retry")
+                    .whenScenarioStateIs("successful retrieval")
+                    .willReturn(
+                        aResponse()
+                            .withHeader("Content-Type", APPLICATION_JSON)
+                            .withStatus(200)
+                            .withBody(responseBody)
+                    )
+            )
 
-        val downloader = AnimePlanetDownloader(testAnimePlanetConfig)
+            val downloader = AnimePlanetDownloader(testAnimePlanetConfig)
 
-        // when
-        val result = runBlocking {
-            downloader.download(id.toString()) {
+            // when
+            val result = downloader.download(id.toString()) {
                 shouldNotBeInvoked()
             }
-        }
 
-        // then
-        assertThat(result).isEqualTo(responseBody)
+            // then
+            assertThat(result).isEqualTo(responseBody)
+        }
     }
 
     @Test
     fun `invoke lambda on finding a dead entry`() {
-        // given
-        val testConfig = object: MetaDataProviderConfig by MetaDataProviderTestConfig {
-            override fun hostname(): Hostname = "localhost"
-            override fun buildDataDownloadLink(id: String): URI = URI("http://localhost:$port/anime/$id")
-        }
+        runBlocking {
+            // given
+            val testConfig = object : MetaDataProviderConfig by MetaDataProviderTestConfig {
+                override fun hostname(): Hostname = "localhost"
+                override fun buildDataDownloadLink(id: String): URI = URI("http://localhost:$port/anime/$id")
+            }
 
-        val id = "black-clover"
+            val id = "black-clover"
 
-        serverInstance.stubFor(
-            get(urlPathEqualTo("/anime/$id")).willReturn(
-                aResponse()
-                    .withHeader("Content-Type", APPLICATION_JSON)
-                    .withStatus(200)
-                    .withBody(loadTestResource("downloader_tests/dead-entry.html"))
+            serverInstance.stubFor(
+                get(urlPathEqualTo("/anime/$id")).willReturn(
+                    aResponse()
+                        .withHeader("Content-Type", APPLICATION_JSON)
+                        .withStatus(200)
+                        .withBody(loadTestResource("downloader_tests/dead-entry.html"))
+                )
             )
-        )
 
-        val downloader = AnimePlanetDownloader(testConfig)
+            val downloader = AnimePlanetDownloader(testConfig)
 
-        var deadEntryHasBeenInvoked = false
+            var deadEntryHasBeenInvoked = false
 
-        // when
-        val result = runBlocking {
-            downloader.download(id) {
+            // when
+            val result = downloader.download(id) {
                 deadEntryHasBeenInvoked = true
             }
-        }
 
-        // then
-        assertThat(result).isEmpty()
-        assertThat(deadEntryHasBeenInvoked).isTrue()
+            // then
+            assertThat(result).isEmpty()
+            assertThat(deadEntryHasBeenInvoked).isTrue()
+        }
     }
 }
