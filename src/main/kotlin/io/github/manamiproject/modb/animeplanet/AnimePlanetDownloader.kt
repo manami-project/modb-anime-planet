45ca617cb4f6c0c1381f4282ffad31f8d16a4425
manami-project <13179375+manami-project@users.noreply.github.com>
2023-08-27 18:33:15 +0200

    Adjustments for modb-core 9.0.0

--- src/main/kotlin/io/github/manamiproject/modb/animeplanet/AnimePlanetDownloader.kt
+++ src/main/kotlin/io/github/manamiproject/modb/animeplanet/AnimePlanetDownloader.kt
@@ -7,8 +7,6 @@ import io.github.manamiproject.modb.core.downloader.Downloader
 import io.github.manamiproject.modb.core.extensions.EMPTY
 import io.github.manamiproject.modb.core.httpclient.DefaultHttpClient
 import io.github.manamiproject.modb.core.httpclient.HttpClient
-import io.github.manamiproject.modb.core.httpclient.retry.RetryBehavior
-import io.github.manamiproject.modb.core.httpclient.retry.RetryableRegistry
 import io.github.manamiproject.modb.core.logging.LoggerDelegate
 import io.github.manamiproject.modb.core.random
 import kotlinx.coroutines.withContext
@@ -26,17 +24,12 @@ public class AnimePlanetDownloader(
     private val httpClient: HttpClient = DefaultHttpClient(isTestContext = config.isTestContext()),
 ) : Downloader {
 
-    init {
-        registerRetryBehavior()
-    }
-
     override suspend fun download(id: AnimeId, onDeadEntry: suspend (AnimeId) -> Unit): String = withContext(LIMITED_NETWORK) {
         log.debug { "Downloading [animePlanetId=$id]" }
 
         val response = httpClient.get(
             url = config.buildDataDownloadLink(id).toURL(),
             headers = mapOf("host" to listOf("www.${config.hostname()}")),
-            retryWith = config.hostname(),
         )
 
         check(response.body.isNotBlank()) { "Response body was blank for [animePlanetId=$id] with response code [${response.code}]" }
@@ -50,19 +43,6 @@ public class AnimePlanetDownloader(
             200 -> response.body
             else -> throw IllegalStateException("Unable to determine the correct case for [animePlanetId=$id], [responseCode=${response.code}]")
         }
-    }
-
-    private fun registerRetryBehavior() {
-        val retryBehaviorConfig = RetryBehavior(
-            waitDuration = { random(4000, 8000).toDuration(MILLISECONDS) },
-            isTestContext = config.isTestContext(),
-        ).apply {
-            addCase {
-                listOf(429, 500, 502, 521, 525).contains(it.code)
-            }
-        }
-
-        RetryableRegistry.register(config.hostname(), retryBehaviorConfig)
     }
 
     private companion object {
